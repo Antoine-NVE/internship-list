55416310584a3d3f48df3b63ee9c91243bfd0642
Antoine-NVE <antoinenavette@gmail.com>
2024-06-12 11:23:39 +0200

    Mise en place du serveur

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('Connexion à MongoDB réussie'))
+    .catch((error) => console.log('Connexion à MongoDB échouée : ' + error));
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('Le serveur tourne sur le port ' + port);
+});
