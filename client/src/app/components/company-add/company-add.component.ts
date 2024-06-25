b4d980d39699dcf6d22ae6dd9ecfaf84aee1b677
Antoine-NVE <antoinenavette@gmail.com>
2024-06-25 15:04:22 +0200

    Création des components utiles à la gestion des entreprises

---
+++ client/src/app/components/company-add/company-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-add.component.html',
+  styleUrl: './company-add.component.css'
+})
+export class CompanyAddComponent {
+
+}
