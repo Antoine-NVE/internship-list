382fb6d040d7a95a9209541e221ff57f15fc4217
Antoine-NVE <antoinenavette@gmail.com>
2024-07-02 13:21:31 +0200

    Création des components pour créer et modifier des contacts

---
+++ client/src/app/components/contact/contact-add/contact-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-add.component.html',
+  styleUrl: './contact-add.component.css'
+})
+export class ContactAddComponent {
+
+}
