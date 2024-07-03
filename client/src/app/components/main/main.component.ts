066d1af895cb9cb0a7a50bfa776c6502fe363998
Antoine-NVE <antoinenavette@gmail.com>
2024-07-03 12:37:22 +0200

    Ajout d'une fonction de tri par nom d'entreprise

--- client/src/app/components/main/main.component.ts
+++ client/src/app/components/main/main.component.ts
@@ -12,13 +12,19 @@ import { RouterLink } from '@angular/router';
     styleUrl: './main.component.css',
 })
 export class MainComponent implements OnInit {
+    public unsortedCompanies: Company[] = [];
     public companies: Company[] = [];
+
+    public companyNameSortingMode: 0 | 1 | 2 = 0;
 
     constructor(private companyService: CompanyService) {}
 
     ngOnInit(): void {
         this.companyService.readAll().subscribe({
-            next: (companies) => (this.companies = companies),
+            next: (companies) => {
+                this.unsortedCompanies = companies;
+                this.companies = [...this.unsortedCompanies]; // Permet de faire une copie indépendante
+            },
             error: (error) => console.error(error),
         });
     }
@@ -27,9 +33,51 @@ export class MainComponent implements OnInit {
     public deleteCompany(id: string) {
         this.companyService.delete(id).subscribe({
             // Après la suppression en base, on supprime l'entreprise de notre array, sans nouvelle requête
-            next: (message) => (this.companies = this.companies.filter((company) => company._id !== id)),
+            next: (message) => {
+                this.companies = this.companies.filter((company) => company._id !== id);
+                this.unsortedCompanies = this.unsortedCompanies.filter((company) => company._id !== id);
+            },
             error: (error) => console.error(error),
         });
+    }
+
+    public sortByName() {
+        // On change le mode de trie
+        this.companyNameSortingMode++;
+        if (this.companyNameSortingMode > 2) {
+            this.companyNameSortingMode = 0;
+        }
+
+        // On trie en fonction de la situation
+        switch (this.companyNameSortingMode) {
+            case 0:
+                this.companies = [...this.unsortedCompanies];
+                break;
+
+            case 1:
+                this.companies.sort((a, b) => {
+                    if (a.name < b.name) {
+                        return -1;
+                    } else if (a.name > b.name) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+                break;
+
+            case 2:
+                this.companies.sort((a, b) => {
+                    if (a.name < b.name) {
+                        return 1;
+                    } else if (a.name > b.name) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                });
+                break;
+        }
     }
 
     // Permet de savoir le nombre de contacts
